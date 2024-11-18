0a76728218acbe78b18958e875be279671c25cff
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-11-18 14:09:04 +0400

    add stripes animation

--- js/script.js
+++ js/script.js
@@ -4,12 +4,22 @@ import {
 } from "./timeOutMiddleware.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const handleHeadlinePriceChange = () => {
+  const handleHeadlinePriceAnimation = () => {
     const headlinePrice = document.getElementById("headline-price");
 
-    addTimeOutMiddleware(headlinePrice, "show", 3000);
-    removeTimeOutMiddleware(headlinePrice, "show", 6000);
+    addTimeOutMiddleware(headlinePrice, "show", 1000);
+    removeTimeOutMiddleware(headlinePrice, "show", 3000);
   };
 
-  handleHeadlinePriceChange();
+  const handleStripesAnimation = () => {
+    const stripes = document.querySelectorAll(".stripes div");
+    for (let i = 0; i < stripes.length; i++) {
+      let delay = 50 * i;
+      const element = stripes[i];
+      addTimeOutMiddleware(element, "show", 1500 + delay);
+    }
+  };
+
+  handleHeadlinePriceAnimation();
+  handleStripesAnimation();
 });
